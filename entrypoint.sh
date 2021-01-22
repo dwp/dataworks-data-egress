c5b5533b327363e8e838b8ad99a0399551b3c31c
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2021-01-22 22:01:59 +0000

    pr comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,15 @@ export NO_PROXY="${non_proxied_endpoints},${dks_fqdn}"
 
 export ACM_KEY_PASSWORD=$(uuidgen -r)
 echo "Retrieving acm certs"
-acm-cert-retriever \
+sudo acm-cert-retriever \
 --acm-cert-arn "${acm_cert_arn}" \
 --acm-key-passphrase "$ACM_KEY_PASSWORD" \
 --private-key-alias "${private_key_alias}" \
 --truststore-aliases "${truststore_aliases}" \
 --truststore-certs "${truststore_certs}" >> /var/log/acm-cert-retriever.log 2>&1
 
-cd /etc/pki/ca-trust/source/anchors/
-touch analytical_ca.pem
+sudo cd /etc/pki/ca-trust/source/anchors/
+sudo touch analytical_ca.pem
 
 TRUSTSTORE_ALIASES="${truststore_aliases}"
 for F in $(echo $TRUSTSTORE_ALIASES | sed "s/,/ /g"); do
