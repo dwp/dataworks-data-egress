6c9f1819b9b9fe2c012376849dde543090fe196d
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2021-01-22 21:22:49 +0000

    pem generation to communicate to dks

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,5 @@ WORKDIR /app
 RUN apk --update --no-cache add gcc musl-dev libffi-dev openssl-dev
 RUN python setup.py install
 
-ENTRYPOINT ["python", "/usr/local/bin/sqs-listener"]
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["python", "/usr/local/bin/sqs-listener"]
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+export HTTP_PROXY="http://${internet_proxy}:3128"
+export HTTPS_PROXY="$HTTP_PROXY"
+export NO_PROXY="${non_proxied_endpoints},${dks_fqdn}"
+
+ACM_KEY_PASSWORD=$(uuidgen -r)
+echo "Retrieving acm certs"
+acm-cert-retriever \
+--acm-cert-arn "${acm_cert_arn}" \
+--acm-key-passphrase "$ACM_KEY_PASSWORD" \
+--private-key-alias "${private_key_alias}" \
+--truststore-aliases "${truststore_aliases}" \
+--truststore-certs "${truststore_certs}" >> /var/log/acm-cert-retriever.log 2>&1
+
+cd /etc/pki/ca-trust/source/anchors/
+touch analytical_ca.pem
+
+TRUSTSTORE_ALIASES="${truststore_aliases}"
+for F in $(echo $TRUSTSTORE_ALIASES | sed "s/,/ /g"); do
+ (sudo cat "$F.crt"; echo) >> analytical_ca.pem;
+done
+
+exec "${@}"
