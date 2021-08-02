c25f2f9fdffd210bec25854c719f28a3b03c84e3
Umair <umair@example.com>
2021-08-02 14:05:00 +0100

    updated

--- src/main/kotlin/uk/gov/dwp/dataworks/egress/services/impl/DataServiceImpl.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/egress/services/impl/DataServiceImpl.kt
@@ -93,13 +93,13 @@ class DataServiceImpl(
                     val request = if (wasEncryptedByHtme(metadata) && specification.rewrapDataKey)
                     {
                         val(encryptingKeyID, reWrappingKey) = fetchReWrappingKeyParameter(specification)
-                        logger.info("ReWrapping public Key received", "public" to "PUBLIC")
+                        logger.info("ReWrapping publicK received", "public" to "PUBLIC")
                         val reWrappedDataKey = reWrapDataKey(
                             metadata[ENCRYPTING_KEY_ID_METADATA_KEY],
                             metadata[CIPHERTEXT_METADATA_KEY],
                             reWrappingKey
                         )
-                        logger.info("ReWrapped data key received", "datak" to "DATAKEY")
+                        logger.info("ReWrapped datak  received", "datak" to "DATAK")
                         putObjectRequestWithReWrappedKeyAsEncryptionMetadata(specification, key, encryptingKeyID,
                             String(reWrappedDataKey), metadata)
                     }
